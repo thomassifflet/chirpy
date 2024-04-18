e3180059b5a7823d0f6b7e7a75cd3eed7c5cbc6c
thomassifflet <thomas.sifflet@gmail.com>
2024-04-18 21:12:47 +0200

    added some handlers to the server

--- main.go
+++ main.go
@@ -18,9 +18,18 @@ func middlewareCors(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
+
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	})
+	fmt.Println("Started server on port 8080..")
 	log.Fatal(http.ListenAndServe(":8080", corsMux))
-	fmt.Println("Hello world")
+
 }
