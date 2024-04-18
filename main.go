95d4f5feb411d1bf170a44102f23c9d7e5a2c091
thomassifflet <thomas.sifflet@gmail.com>
2024-04-18 21:57:39 +0200

    add /reset and /metrics endpoints

--- main.go
+++ main.go
@@ -6,6 +6,36 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+	reset          (int)
+}
+
+func (cfg *apiConfig) middleWareMetricsIncrement(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetMetrics() {
+	cfg.fileserverHits = 0
+}
+
+func (cfg *apiConfig) handleResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.resetMetrics()
+	hitsCount := fmt.Sprintf("Hits count has been reset")
+	w.Header().Set("Content-Type", "text/plain charset=utf-8")
+	w.Write([]byte(hitsCount))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	hitsCount := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Header().Set("Content-Type", "text/plain charset=utf-8")
+	w.Write([]byte(hitsCount))
+
+}
+
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -22,9 +52,12 @@ func middlewareCors(next http.Handler) http.Handler {
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
+	apiCfg := apiConfig{}
 
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+	mux.Handle("/app/", apiCfg.middleWareMetricsIncrement(http.StripPrefix("/app/", fileServer)))
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleResetMetrics)
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
